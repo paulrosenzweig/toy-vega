e072b5b246e6d24b3f6b0512bbfa857987cf6688
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2021-06-30 11:29:15 -0400

    hideous, but something is working

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import * as d3 from "d3";
+
 import example from "./simple-example.json";
 
 function parse(specification) {
@@ -42,7 +44,7 @@ function parse(specification) {
       params: {
         operation: "extent",
         field: domain.field,
-        data: dataNodes[domain.data],
+        data: dataNodes.get(domain.data),
       },
     };
 
@@ -96,6 +98,7 @@ function parse(specification) {
       params: {
         type,
         attributes,
+        data,
       },
     };
     nodes.push(node);
@@ -103,10 +106,72 @@ function parse(specification) {
 
   nodes.push({
     type: "render",
-    params: { markNodes: nodes.filter((n) => n.type === "mark") },
+    params: {
+      markNodes: nodes.filter((n) => n.type === "mark"),
+      widthNode,
+      heightNode,
+    },
   });
 
   return nodes;
 }
 
-console.log(parse(example));
+function render(specification, element) {
+  const dagNodes = parse(specification);
+  const {
+    params: { markNodes, widthNode, heightNode },
+  } = dagNodes.find((node) => node.type === "render");
+  const svg = d3
+    .create("svg")
+    .attr("viewBox", `0 0 ${widthNode.value} ${heightNode.value}`);
+  for (const { params } of markNodes) {
+    const { type: markType, data, attributes } = params;
+    for (const row of data.params.values) {
+      const markItem = svg.append(markType);
+      for (const { name, value } of attributes) {
+        let valueForAttr;
+        if (value.type === "operator") {
+          valueForAttr = value.value;
+        } else if (value.type === "data_manipulation") {
+          const { field, operation, scale: scaleNode } = value.params;
+          if (operation === "call_scale") {
+            const datum = row[field];
+            const scale = scaleForScaleNode(scaleNode);
+            valueForAttr = scale(datum);
+          }
+        }
+        markItem.attr(name, valueForAttr);
+      }
+    }
+  }
+  element.append(svg.node());
+}
+
+function scaleForScaleNode({
+  params: { type: scaleType, domain: domainNode, range: rangeNode },
+}) {
+  if (scaleType !== "linear") throw "Only linear scales are supported";
+
+  const domain = resolveNodeValue(domainNode);
+  const range = resolveNodeValue(rangeNode);
+  return d3.scaleLinear().domain(domain).range([0, range]);
+}
+
+function resolveNodeValue({ type: nodeType, value, params }) {
+  if (nodeType === "data_manipulation") {
+    const { operation, field, data } = params;
+    switch (operation) {
+      case "extent":
+        return d3.extent(data.params.values, (d) => d[field]);
+        break;
+      default:
+        throw `Can't handle data manipulation operation: ${operation}`;
+    }
+  } else if (nodeType === "operator") {
+    return value;
+  } else {
+    throw `Can't handle ${nodeType}`;
+  }
+}
+
+render(example, document.getElementById("chart"));
